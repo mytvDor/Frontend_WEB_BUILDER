649b6bccae30dc4d2db0c7456efae81b1fb6525d
sumit <mytvdor@gmail.com>
2025-01-13 11:40:31 +0530

    start again

--- src/Home.jsx
+++ src/Home.jsx
@@ -34,7 +34,7 @@ export default function DarkLandingPage() {
           <div className="container mx-auto">
             <div className="flex flex-col items-center space-y-4 text-center">
               <div className="space-y-2">
-                <h1 className="text-3xl mb-14 font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none bg-clip-text text-transparent bg-gradient-to-r from-lime-400 to-teal-400">
+                <h1 className=" text-5xl mb-14 font-bold tracking-tighter sm:text-5xl md:text-5xl lg:text-9xl/none bg-clip-text text-transparent bg-gradient-to-r from-lime-400 to-teal-400">
                   Deploy at the Speed of Innovation
                 </h1>
                 <p className="mx-auto  max-w-[700px] text-zinc-400 md:text-xl" >
